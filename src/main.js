a1b64a3919439328d665c4da7fded467bc9be930
Dy1anL1 <131739925+Dy1anL1@users.noreply.github.com>
2025-08-12 18:19:03 +1000

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './assets/main.css'
+import './style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
